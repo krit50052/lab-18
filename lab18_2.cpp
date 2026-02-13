f2d1bfc689d32cd46e1ac41927271ad067970708
krit <krittapasboonruang@gmail.com>
2026-02-13 20:15:31 +0700

    lab18_2 completd

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 struct Rect{
-	double x,y,w,h;
+    double x, y, w, h;
 };
 
-double overlap(){
+double overlap(Rect* R1, Rect* R2){
 
-
-
+    double overlap_width = min(R1->x + R1->w, R2->x + R2->w) - max(R1->x, R2->x);
+    double overlap_height =  min(R1->y,R2->y) - max(R1->y - R1->h, R2->y - R2->h);
+    
+    if(overlap_width <= 0 || overlap_height <= 0){
+        return 0;
+    }
+    return overlap_width * overlap_height;
 }
